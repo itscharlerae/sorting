a054227cc5d8b291b44dcfee0f8aafb2ae7ea9be
charle.rae <itscharlerae@gmail.com>
2020-12-04 23:35:06 -0400

    adds binary search

--- basic-sorts.js
+++ basic-sorts.js
@@ -1,32 +1,32 @@
 
-// function binarySearch(array, answer) {
-//     let start = 0;
-//     let end = array.length -1;
-//     let middle = Math.floor((start + end)/2);
+function binarySearch(array, answer) {
+    let start = 0;
+    let end = array.length -1;
+    let middle = Math.floor((start + end)/2);
 
-//     while (array[middle] !== answer && start <= end) {
-//         console.log(start, middle, end);
+    while (array[middle] !== answer && start <= end) {
+        console.log(start, middle, end);
 
-//         if (answer === array[middle]) middle;
+        if (answer === array[middle]) middle;
 
-//         if (answer < array[middle]) {
-//             end = middle - 1;
-//         } else {
-//             start = middle + 1;
-//         }
-//         middle = Math.floor((start + end)/2);
+        if (answer < array[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end)/2);
 
-//     } return -1; 
-// } 
+    } return -1; 
+} 
 
 
-let people = ['rosemary', 'alex', 'josh', 'polina', 'david', 'anna'];
+// let people = ['rosemary', 'alex', 'josh', 'polina', 'david', 'anna'];
 
-function allCaps(arr) {
-    let newArr = [];
-    arr.forEach(name => {
-        newArr.push(name.toUpperCase())
-    }); return newArr;
-}
+// function allCaps(arr) {
+//     let newArr = [];
+//     arr.forEach(name => {
+//         newArr.push(name.toUpperCase())
+//     }); return newArr;
+// }
 
-console.log(allCaps(people));
+// console.log(allCaps(people));
