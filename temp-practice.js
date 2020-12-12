3a6a828c4343e69f24d9e9a36eaf229cf658e9e2
charle.rae <itscharlerae@gmail.com>
2020-12-12 18:04:11 -0400

    edits gimmiePairs function for unique pairing

--- temp-practice.js
+++ temp-practice.js
@@ -3,7 +3,7 @@ const puppies = ['shoo', 'chow', 'biscuit', 'mr. fluff', 'kevin'];
 
 function gimmiePairs(array) {
     for (let i = 0; i < array.length; i++) {
-        for (let k = 1; k < array.length; k++) {
+        for (let k = i + 1; k < array.length; k++) {
             console.log( `${array[i]} and ${array[k]}`);
         }
     }
