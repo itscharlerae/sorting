c1e37627f77b6dd6642140320aaa02f69d3b1155
charle.rae <itscharlerae@gmail.com>
2020-12-14 00:46:58 -0400

    adds cbReturn function

--- temp-practice.js
+++ temp-practice.js
@@ -10,3 +10,22 @@ function gimmiePairs(array) {
 }
 
 gimmiePairs(puppies);
+
+
+function cbReturn(num, cb1, cb2) {
+    cb1(num);
+    cb2(num);
+    if (cb1 > cb2) return cb1;
+    return cb2;
+}
+
+function doubleIt(num) {
+    return num * 2;
+}
+
+function squareIt(num) {
+    return num * num;
+}
+
+console.log(cbReturn(5, doubleIt, squareIt));
+console.log(cbReturn(1, doubleIt, squareIt));
